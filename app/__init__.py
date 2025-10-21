c0bcf4ef9811513b90e9afbeb777bcb64ad9fe07
SoldierrBoy <ivan.tumchuk@icloud.com>
2025-10-21 14:55:45 +0300

    Finish Lab 4: Add theme switching functionality

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ app.register_blueprint(users_bp, url_prefix='/users')
 from app.products.views import products_bp
 app.register_blueprint(products_bp, url_prefix='/products')
 
-# ------------------------------
+app.secret_key = 'a_very_secret_and_long_random_string'
 
 # Імпортуємо основні маршрути (resume, contacts)
 from app import views
